e42f9c28723af11edac2b0edc83bcc95152f7314
Hitarth <hitarthchudgar@gmail.com>
2020-11-29 00:49:26 -0500

    trending updates

--- views/trending.php
+++ views/trending.php
@@ -1,7 +1,7 @@
 <?php ?>
 
 <h1>Trending Songs</h1>
-
+<div class="row d-flex justify-content-center">
 
 <!--  TESTING
 <h1><?php echo $params['songs']['items'][0]['track']['name']; ?></h1>
@@ -10,24 +10,17 @@
 <img src="<?php echo $params['tracks']['items'][0]['album']['images'][0]['url']; ?>"  height="300px">
 -->
 
-
+echo"<div class = 'row'>";
     <?php foreach($params['songs']['items'] as $songs){
+        echo'<div class = “col-md-3”>';
         echo '<div class = "trending-div">';
-        echo '<h2 class="test">SONGS</h2>';
-        echo "<h5>".$songs['track']['name']."</h5>";
-        echo '<h3>ARTISTS</h3>';
-        echo "<h5>".$songs['track']['album']['artists'][0]['name']."</h5>";
-        $source= $songs['track']['album']['images'][0]['url'];
-        echo "<img src='".$source."' height = '400'>";
+            echo "<h5>".$songs['track']['name']."</h5>";
+            echo "<h5>".$songs['track']['album']['artists'][0]['name']."</h5>";
+            $source= $songs['track']['album']['images'][0]['url'];
+            echo "<img src='".$source."' height='250'>";
         $source2 = $songs['track']['preview_url'];
         echo "<br>";
         echo "<video width = '250' height='75' controls=''  name='media'><source src='".$source2."' type='audio/mpeg'></video>";
         echo '</div>';
-
-
-
-
-
     } ?>
-
-
+</div>
