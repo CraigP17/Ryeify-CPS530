ff43a30af2c3a49ec034626863a5a026a529e3c4
Craig <craigpinto17@gmail.com>
2020-11-29 19:18:31 -0500

    Updated error view

--- views/error404.php
+++ views/error404.php
@@ -1,9 +1,11 @@
-<?php
-
-?>
+<?php ?>
 
 <div class="row">
     <div class="col-12 text-center">
-        <h1>Uh oh...Page Not Found!</h1>
+        <h1 class="mt-4 mb-4">404 </h1>
+
+        <h2 class="mb-4">Uh Oh... Something Went Wrong!</h2>
+
+        <a href="/" class="btn btn-success btn-lg" role="button" aria-pressed="true">Take me back home</a>
     </div>
 </div>
