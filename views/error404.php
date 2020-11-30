2f45075bb14892b74086b5fb36dc6c2c2a7c1d92
Hitarth <hitarthchudgar@gmail.com>
2020-11-30 00:24:40 -0500

    made minor changes to 404

--- views/error404.php
+++ views/error404.php
@@ -4,8 +4,8 @@
     <div class="col-12 text-center">
         <h1 class="mt-4 mb-4">404 </h1>
 
-        <h2 class="mb-4">Uh Oh... Something Went Wrong!</h2>
+        <h2 class="mb-4">Uh Oh... Looks like you hit the forbidden playlist</h2>
 
-        <a href="/" class="btn btn-success btn-lg" role="button" aria-pressed="true">Take me back home</a>
+        <a href="/" class="btn btn-success btn-lg" role="button" aria-pressed="true">Take me back to Music!</a>
     </div>
 </div>
