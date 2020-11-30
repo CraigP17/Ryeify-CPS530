8c43aac7490b9096f13a75ec61e50d87bcd5ade6
manpreet rajpal <manpreetrajpal7@gmail.com>
2020-11-30 01:05:28 -0500

    login page styling **still need to fix background img error

--- views/login.php
+++ views/login.php
@@ -5,29 +5,67 @@ use app\models\LoginModel;
 
 ?>
 
+<head>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!--Bootsrap 4 CDN-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        
+        <!--Fontawesome CDN-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="../css/login.css">
+
+</head>
+
 <h1>Login Page</h1>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign In</h3>
+            </div>
+            <div class="card-body">
+                <form action="/login" method="post">
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                        </div>
+                        <label for="email"></label><br>
+                        <input type="text" id="email" name="email" class="form-control" placeholder="email" value="<?= $model->email ?>"
+                            class="form-control <?= $model->hasError('email') ? ' is-invalid': '' ?>">
+                        <div class="invalid-feedback">
+                            <?= $model->getFirstError('email') ?>
+                        </div>
+                    </div>
 
-<form action="/login" method="post">
-    <div class="form-group">
-        <label for="email">Email:</label><br>
-        <input type="text" id="email" name="email" value="<?= $model->email ?>"
-               class="form-control <?= $model->hasError('email') ? ' is-invalid': '' ?>">
-        <div class="invalid-feedback">
-            <?= $model->getFirstError('email') ?>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        </div>
+                        <label for="password"></label><br>
+                        <input type="password" id="password" placeholder="password" name="password" value=""
+                            class="form-control <?= $model->hasError('password') ? ' is-invalid': '' ?>">
+                        <div class="invalid-feedback">
+                            <?= $model->getFirstError('password') ?>
+                        </div>
+                    </div>
+                    <div class="row align-items-center remember">
+						<input type="checkbox">Remember Me
+					</div>
+                    <input type="submit" value="Submit" class="btn float-right login_btn">
+                </form>
+            </div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-center links">
+                    Don't have an account?<a href="/register" >Sign Up</a>
+                </div>
+            </div>
         </div>
     </div>
-
-    <div class="form-group">
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" value=""
-               class="form-control <?= $model->hasError('password') ? ' is-invalid': '' ?>">
-        <div class="invalid-feedback">
-            <?= $model->getFirstError('password') ?>
-        </div>
-    </div>
-
-    <input type="submit" value="Submit">
-</form>
-<br>
-<a href="/register" class="btn btn-secondary">Register</a>
+</div>
 
