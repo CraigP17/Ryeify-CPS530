57a9c80760fc16a7abe1bba8bbd83cd94ca47a48
Hitarth <hitarthchudgar@gmail.com>
2020-11-30 00:27:02 -0500

    added image

--- views/error404.php
+++ views/error404.php
@@ -1,9 +1,9 @@
 <?php ?>
 
-<div class="row">
+<div class="row d-flex justify-content-center">
     <div class="col-12 text-center">
-        <h1 class="mt-4 mb-4">404 </h1>
-
+        <h1 class="mt-4 mb-4">404</h1>
+        <img src="/img/404.png" alt="Astronaut Floating">
         <h2 class="mb-4">Uh Oh... Looks like you hit the forbidden playlist</h2>
 
         <a href="/" class="btn btn-success btn-lg" role="button" aria-pressed="true">Take me back to Music!</a>
