3bcfc7df40307278ef754022702347c1eae54857
Craig <craigpinto17@gmail.com>
2020-12-05 02:15:59 -0500

    Updated user table DB Create users

--- dbstructure/dbstructure.php
+++ dbstructure/dbstructure.php
@@ -1,18 +1,16 @@
 <?php
 
 // Create the Users Table
-$SQL = "CREATE TABLE users (
+$SQL = "
+        CREATE TABLE users (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 email VARCHAR(255) NOT NULL,
                 fname VARCHAR(55) NOT NULL,
                 lname VARCHAR(55) NOT NULL,
                 password VARCHAR(512) NOT NULL,
-                status TINYINT NOT NULL,
+                spotify_connected TINYINT DEFAULT 0,
+                spotify_refresh_token varchar(255) NOT NULL DEFAULT '',
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            )  ENGINE=INNODB;";
+            )  ENGINE=INNODB;
+        ";
 
-$updateUser = "ALTER TABLE users 
-                    DROP COLUMN status, 
-                    ADD spotify_connected tinyint DEFAULT 0 AFTER password ,
-                    ADD spotify_refresh_token varchar(255) NOT NULL DEFAULT ''
-                    ;";
