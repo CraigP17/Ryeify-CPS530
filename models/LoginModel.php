434264829194467ad0ccbe92396df94a27a02585
Craig <craigpinto17@gmail.com>
2020-12-05 02:43:05 -0500

    Added comments to Models

--- models/LoginModel.php
+++ models/LoginModel.php
@@ -5,12 +5,23 @@ namespace app\models;
 
 
 use app\core\Model;
+use Application;
 
 class LoginModel extends Model
 {
+    // LoginModel that extends Model class in core
+    // Used for user login to the app
+
     public $email = '';
     public $password = '';
 
+    /**
+     * Returns the rules to check for each input
+     * Form contains 2 inputs: email and password. Email, check that it was entered (Required) and email format (Email)
+     * Fulfils abstract method defined in Model->rules()
+     *
+     * @return array[]|mixed
+     */
     public function rules()
     {
         return [
@@ -19,20 +30,31 @@ class LoginModel extends Model
         ];
     }
 
+    /**
+     * Returns whether login is successful where:
+     *      email exists
+     *      password matches and is correct
+     *
+     * @return bool
+     */
     public function login()
     {
+        // Find if there is a user existing with entered email
         $user = RegisterModel::findOneUser(['email' => $this->email]);
+
         if (!$user)
         {
             $this->addPubError('email', 'User does not exist with this email');
             return false;
         }
+
+        // Verify whether password matches the found user's password
         if (!password_verify($this->password, $user->password))
         {
             $this->addPubError('password', 'Incorrect Password!');
             return false;
         }
 
-        return \Application::$app->login($user);
+        return Application::$app->login($user);
     }
 }
--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -8,28 +8,56 @@ use app\core\DBModel;
 
 class RegisterModel extends DBModel
 {
+    // RegisterModel that extends Model class in core
+    // Used for user registration to the app
+
     public $fname;
     public $lname;
     public $email;
     public $password;
     public $confirmPassword;
 
+    /**
+     * Returns table name corresponding to registration within the DB
+     * Fulfils abstract method defined in Model->tableName()
+     *
+     * @return string
+     */
     public function tableName(): string
     {
         return 'users';
     }
 
+    /**
+     * Returns the DB column names that correspond to user registration
+     * Fulfils abstract method defined in Model->attributes()
+     *
+     * @return string[]
+     */
     public function attributes(): array
     {
         return ['fname', 'lname', 'email', 'password'];
     }
 
+    /**
+     * Registers the user to the DB and returns whether registration was successful
+     * Hashes/Encrypts password and then uses Model->save()
+     *
+     * @return bool
+     */
     public function register()
     {
         $this->password = password_hash($this->password, PASSWORD_DEFAULT);
         return $this->save();
     }
 
+    /**
+     * Returns the rules to check for each input
+     * Form contains 5 inputs. email: check it was entered (Required), email format (Email) and is unique (UNIQUE)
+     * Fulfils abstract method defined in Model->rules()
+     *
+     * @return array[]|mixed
+     */
     public function rules()
     {
         return [
@@ -41,11 +69,22 @@ class RegisterModel extends DBModel
         ];
     }
 
+    /**
+     * Returns the primary key of the DB users (id)
+     *
+     * @return string
+     */
     public function primaryKey(): string
     {
         return 'id';
     }
 
+    /**
+     * Returns the user's username which is their first name + last name
+     * Used in navbar php file to echo a user's name when they are signed in
+     *
+     * @return string
+     */
     public function userName()
     {
         return $this->fname . ' ' . $this->lname;
