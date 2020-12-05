d052b1feec2011525890f95d4db22daa3d410ef3
Craig <craigpinto17@gmail.com>
2020-12-04 23:59:45 -0500

    Added comments to Application file

--- core/Application.php
+++ core/Application.php
@@ -30,14 +30,16 @@ class Application
         $this->userClass = \app\models\RegisterModel::class;
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
-        $this->request = new Request();
-        $this->response = new Response();
-        $this->session = new Session();
-        $this->router = new Router($this->request, $this->response);
+        $this->request = new Request();    // Gets URL path parameters
+        $this->response = new Response();  // New Response class, used for redirection and setting status codes
+        $this->session = new Session();    // Instantiates new Session session_start()
+        $this->router = new Router($this->request, $this->response); // Uses URL path to route to specific controller
 
+        // Instantiates PDO connection to DB loading configuration
         $this->config = parse_ini_file('../private/config.ini');;
         $this->db = new Database($this->config);
 
+        // Adds Spotify client token if needed, to connect to API
         $this->checkShopifyToken();
 
         // Get logged in user from session
@@ -57,6 +59,9 @@ class Application
     }
 
 
+    /**
+     * Runs the program, echos the View files from respective controllers
+     */
     public function run()
     {
         echo $this->router->resolve();
@@ -78,6 +83,12 @@ class Application
         $this->controller = $controller;
     }
 
+    /**
+     * Checks for Spotify Client Token exists or needs to be replaced
+     * Require non expired token for connecting to Spotify API
+     *
+     * @return bool whether token retrieval fails, failure of retrieval redirects to /error
+     */
     public function checkShopifyToken()
     {
         $access_token_ready = false;
@@ -149,6 +160,12 @@ class Application
         return true;
     }
 
+    /**
+     * Makes cURL request to /api/token to receive client token from Spotify
+     * Token must be active as its used in every API call to Spotify
+     *
+     * @return bool on whether retrieving the token was successful
+     */
     protected function getShopifyBearerToken()
     {
         $curl = curl_init();
@@ -187,6 +204,14 @@ class Application
         return false;
     }
 
+    /**
+     * When user is logged in and connected to spotify
+     * Makes cURL request to receive their specific user token from spotify
+     *
+     * @param $user_id  // users id from db
+     * @param $token    // users refresh token pulled from db
+     * @return bool     // successful token response from Spotify
+     */
     public function getSpotifyUserToken($user_id, $token)
     {
         $curl = curl_init();
@@ -231,6 +256,12 @@ class Application
         return false;
     }
 
+    /**
+     * Logs in user, checks if they are connected to spotify and retrieves needed keys
+     *
+     * @param $user     // Users id in db
+     * @return bool
+     */
     public function login($user)
     {
         // Save user in session
@@ -251,6 +282,11 @@ class Application
         return true;
     }
 
+    /**
+     * Whether a user is logged in or not
+     *
+     * @return bool
+     */
     public function loggedIn()
     {
         if ($this->user != null)
@@ -260,6 +296,10 @@ class Application
         return false;
     }
 
+    /**
+     * Logs a user out by removing their session keys and saved data
+     * Removes sessions keys for spotify if they have Spotify connected
+     */
     public function logout()
     {
         $this->user = null;
