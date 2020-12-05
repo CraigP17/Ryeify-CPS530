a39b213be1054db2468bfe608946c396c75a7081
Hitarth <hitarthchudgar@gmail.com>
2020-12-05 18:19:54 -0500

    Fixed responsive width still have to figure white space on Register page

--- views/login.php
+++ views/login.php
@@ -6,25 +6,26 @@ use app\models\LoginModel;
 ?>
 
 <head>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="../css/loginregis.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/loginregis.css">
 
 </head>
 
 <h1>Login Page</h1>
 <div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Sign In</h3>
+    <div class="d-flex justify-content-center h-100">
+        <div class="card mt-3">
+            <div class="card-header">
+                <h3>Sign In</h3>
             </div>
             <div class="card-body">
                 <form action="/login" method="post">
@@ -33,7 +34,8 @@ use app\models\LoginModel;
                             <span class="input-group-text"><i class="fas fa-user"></i></span>
                         </div>
                         <label for="email"></label><br>
-                        <input type="text" id="email" name="email" class="form-control" placeholder="email" value="<?= $model->email ?>"
+                        <input type="text" id="email" name="email" class="form-control" placeholder="email"
+                            value="<?= $model->email ?>"
                             class="form-control <?= $model->hasError('email') ? ' is-invalid': '' ?>">
                         <div class="invalid-feedback">
                             <?= $model->getFirstError('email') ?>
@@ -52,17 +54,14 @@ use app\models\LoginModel;
                         </div>
                     </div>
                     <div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
-					</div>
-                    <input type="submit" value="Submit" class="btn float-right login_btn">
+                        <input type="checkbox">Remember Me
+                    </div>
+                    <input type="submit" value="Submit" class="btn btn-primary float-right login_btn" style="color: white">
                 </form>
             </div>
-            <div class="card-footer">
-                <div class="d-flex justify-content-center links">
-                    Don't have an account?<a href="/register">Sign Up</a>
-                </div>
+            <div class="d-flex justify-content-center links pb-2" style="color: #12181c">
+                Don't have an account?<a href="/register">Sign Up</a>
             </div>
         </div>
     </div>
 </div>
-
--- views/register.php
+++ views/register.php
@@ -5,67 +5,62 @@
 
 <h1>Registration Page</h1>
 <div class="register-container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
+    <div class="d-flex justify-content-center h-100">
+        <div class="card mt-3">
             <div class="card-body">
-            <form action="" method="post">
-                <div class="form-group">
-                    <label for="fname"></label><br>
-                    <h6>First Name:</h6>
-                    <input type="text" id="fname" name="fname" placeholder="first name" value="<?= $model->fname ?>"
-                        class="form-control <?= $model->hasError('fname') ? ' is-invalid': '' ?>">
-                    <div class="invalid-feedback">
-                        <?= $model->getFirstError('fname') ?>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="fname"></label><br>
+                        <h6>First Name:</h6>
+                        <input type="text" id="fname" name="fname" placeholder="first name" value="<?= $model->fname ?>"
+                            class="form-control <?= $model->hasError('fname') ? ' is-invalid': '' ?>">
+                        <div class="invalid-feedback">
+                            <?= $model->getFirstError('fname') ?>
+                        </div>
                     </div>
-                </div>
-                <h6>Last Name:</h6>
-                <div class="input-group form-group">
-                    <label for="lname"></label><br>
-                    <input type="text" id="lname" name="lname" placeholder="last name" value="<?= $model->lname ?>"
-                        class="form-control <?= $model->hasError('lname') ? ' is-invalid': '' ?>">
-                    <div class="invalid-feedback">
-                        <?= $model->getFirstError('lname') ?>
+                    <h6>Last Name:</h6>
+                    <div class="input-group form-group">
+                        <label for="lname"></label><br>
+                        <input type="text" id="lname" name="lname" placeholder="last name" value="<?= $model->lname ?>"
+                            class="form-control <?= $model->hasError('lname') ? ' is-invalid': '' ?>">
+                        <div class="invalid-feedback">
+                            <?= $model->getFirstError('lname') ?>
+                        </div>
                     </div>
-                </div>
 
-                <h6>Email:</h6>
-                <div class="input-group form-group">
-                    <label for="email"></label><br>
-                    <input type="text" id="email" name="email" placeholder="email" value="<?= $model->email ?>"
-                        class="form-control <?= $model->hasError('email') ? ' is-invalid': '' ?>">
-                    <div class="invalid-feedback">
-                        <?= $model->getFirstError('email') ?>
+                    <h6>Email:</h6>
+                    <div class="input-group form-group">
+                        <label for="email"></label><br>
+                        <input type="text" id="email" name="email" placeholder="email" value="<?= $model->email ?>"
+                            class="form-control <?= $model->hasError('email') ? ' is-invalid': '' ?>">
+                        <div class="invalid-feedback">
+                            <?= $model->getFirstError('email') ?>
+                        </div>
                     </div>
-                </div>
 
-                <h6>Password</h6>
-                <div class="input-group form-group">
-                    <label for="password"></label><br>
-                    <input type="password" id="password" placeholder="password" name="password" value=""
-                        class="form-control <?= $model->hasError('password') ? ' is-invalid': '' ?>">
-                    <div class="invalid-feedback">
-                        <?= $model->getFirstError('password') ?>
+                    <h6>Password</h6>
+                    <div class="input-group form-group">
+                        <label for="password"></label><br>
+                        <input type="password" id="password" placeholder="password" name="password" value=""
+                            class="form-control <?= $model->hasError('password') ? ' is-invalid': '' ?>">
+                        <div class="invalid-feedback">
+                            <?= $model->getFirstError('password') ?>
+                        </div>
                     </div>
-                </div>
 
-                <h6>Confirm Password</h6>
-                <div class="input-group form-group">
-                    <label for="confirmPassword"></label><br>
-                    <input type="password" id="confirmPassword" placeholder="Confirm Password" name="confirmPassword" value=""
-                        class="form-control <?= $model->hasError('confirmPassword') ? ' is-invalid': '' ?>">
-                    <div class="invalid-feedback">
-                        <?= $model->getFirstError('confirmPassword') ?>
+                    <h6>Confirm Password</h6>
+                    <div class="input-group form-group">
+                        <label for="confirmPassword"></label><br>
+                        <input type="password" id="confirmPassword" placeholder="Confirm Password"
+                            name="confirmPassword" value=""
+                            class="form-control <?= $model->hasError('confirmPassword') ? ' is-invalid': '' ?>">
+                        <div class="invalid-feedback">
+                            <?= $model->getFirstError('confirmPassword') ?>
+                        </div>
                     </div>
-                </div>
-                <div class="card-footer">
-                    <div class="d-flex justify-content-center links">
-                        <input type="submit" value="Submit" class="submit">
-                    </div>
-                </div>
-             </form>
-            </div>  
+                    <input type="submit" value="Submit" class="submit btn btn-primary float-right">
+                </form>
+            </div>
         </div>
     </div>
 </div>
-
-
