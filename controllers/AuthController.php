ab2e3ab63a56b861f3c34b27341655bb5d071175
Craig <craigpinto17@gmail.com>
2020-12-06 16:14:22 -0500

    Added redirect in profile for users not logged in

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -67,23 +67,17 @@ class AuthController extends Controller
 
     public function profile($request, $response)
     {
-        if ($_POST)
-        {
-            // From Profile Data Form
-            $data = $request->getBody();
-            // validate data
-            if (true)
-            {
-                $response->redirect('/');
-                return 0;
-            }
-        }
-
         // GET request
         // return profile view
-        // $_SESSION['spotify_active'] = true;
-
         $params = array();
+
+        // If not logged in, return to login page
+        if (!isset($_SESSION['user']))
+        {
+            $response->redirect('/login');
+            return 0;
+        }
+
         $params['spotify_active'] = false;
 
         if (isset($_SESSION['spotify_active']) && $_SESSION['spotify_active'] == true)
