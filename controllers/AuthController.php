30abaad163fa1da9b42f9055e362cd6462990a15
Craig <craigpinto17@gmail.com>
2020-12-07 05:17:04 -0500

    Removed json file call

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -160,14 +160,10 @@ class AuthController extends Controller
         }
         else
         {
-            // Spotify Account not Account, use defaults
+            // Spotify Account not connected, use defaults
             $tracks_json = file_get_contents("../temp-json/tracks.json");
             $tracks = json_decode($tracks_json, true);
-            $artists_json = file_get_contents("../temp-json/artists.json");
-            $artists = json_decode($artists_json, true);
-
             $params['tracks'] = $tracks;
-            $params['artists'] = $artists;
         }
         return $this->render('personalized', $params);
     }
