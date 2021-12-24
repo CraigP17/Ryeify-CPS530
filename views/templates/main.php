3a05a85c7ed2a15c43d259b30cf7af8e2773a2d3
Craig <craigpinto17@gmail.com>
2021-12-24 01:04:02 -0500

    Added Analytics

--- views/templates/main.php
+++ views/templates/main.php
@@ -38,13 +38,13 @@
 <body>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TKZGDH3QSL"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8JXV0FDC90"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-TKZGDH3QSL');
+        gtag('config', 'G-8JXV0FDC90');
     </script>
 
     <nav class="site-header navbar navbar-expand-lg sticky-top navbar-dark nav-color">
